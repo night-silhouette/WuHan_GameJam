68195ec69211b65790b6c2360f9b663362710ccc
night <631434665@qq.com>
2026-09-03 18:04:17 +0800

    util

--- src/Code/Scene/Component/long_button.gd
+++ src/Code/Scene/Component/long_button.gd
@@ -1,1 +1,16 @@
 extends Node2D
+@onready var Area: Area2D = $Area2D
+@onready var button: Sprite2D = $Button
+
+const BUTTON_NORMAL: Texture2D = preload("uid://dqb2aqy18mg6j")
+const BUTTON_HOVER: Texture2D = preload("uid://d0yx1oj0ky1eo")
+const BUTTON_PRESS: Texture2D = preload("uid://bebkf3y2ggt4q")
+
+
+func _ready() -> void:
+	Area.mouse_entered.connect(func():Util.SetNodeTexture(button,BUTTON_HOVER))
+	Area.mouse_exited.connect(func():Util.SetNodeTexture(button,BUTTON_NORMAL))
+	Util.Area2dConnectClick(Area,func():Util.SetNodeTexture(button,BUTTON_PRESS))
+	Util.Area2dConnectRelease(Area,func():Util.SetNodeTexture(button,BUTTON_HOVER))
+	
+	
--- src/Code/Util/Util.gd
+++ src/Code/Util/Util.gd
@@ -10,23 +10,34 @@ func setTime(time,callback)->SceneTreeTimer:
 	
 	
 #点击area2D,触发回调
-func area2d_connect_click(area2d:Area2D,callback:Callable)->void:
+func Area2dConnectClick(area2d:Area2D,callback:Callable)->void:
 	area2d.input_event.connect(func(obj,event,id):
 		if event is InputEventMouseButton:
 			if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 				callback.call()
 		)
 	
+#松开鼠标
+func Area2dConnectRelease(area2d: Area2D, callback: Callable) -> void:
+	area2d.input_event.connect(func(_viewport, event, _shape_idx):
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and not event.pressed:
+				callback.call()
+	)
 	
-
+#改变节点的texture
+func SetNodeTexture(node: Node, new_texture: Texture2D) -> void:
+	if node and "texture" in node:
+		node.texture = new_texture
 
 
 	
-#tween_fast_to_slow($Sprite2D, "modulate:a", 0.0, 1.0, func():
+#tween_fast_to_slow($Sprite2D, "modulate:a", 0.0, 1.0(时间), func():
 	#print("淡出动画播完了！")
 #)
+
 #callback是补间动画结束触发的回调	
-func tween_fast_to_slow(obj,prop,value,time,callback=func():pass):
+func TweenFastToSlow(obj,prop,value,time,callback=func():pass):
 	var tween=create_tween()
 	tween.set_ease(Tween.EASE_OUT)
 	tween.set_trans(Tween.TRANS_CUBIC)	
@@ -34,5 +45,12 @@ func tween_fast_to_slow(obj,prop,value,time,callback=func():pass):
 	tween.finished.connect(callback)
 	return tween#tween的引用没有了之后,tween.finished.connect(callback)这个的信号也会随之free,所以可以大胆的对这个tween.kill()
 
-
-		
+# callback是补间动画结束触发的回调	
+func TweenSlowToFast(obj, prop, value, time, callback = func(): pass):
+	var tween = create_tween()
+	tween.set_ease(Tween.EASE_IN)
+	tween.set_trans(Tween.TRANS_CUBIC)	
+	tween.tween_property(obj, prop, value, time)
+	tween.finished.connect(callback)
+	return tween
+	
