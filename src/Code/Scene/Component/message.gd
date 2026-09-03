1dcaf87acfa0e15ccf25cc3a8729dee7abf04fb9
Napori232 <3093291282@qq.com>
2026-09-03 18:20:31 +0800

    message完成

--- src/Code/Scene/Component/message.gd
+++ src/Code/Scene/Component/message.gd
@@ -4,7 +4,12 @@ var labfix : = LabelFix
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.MessagePopu.connect(MessagePopu)
+	modulate.a = 0;
 
 func MessagePopu(message:String):
 	labfix.auto_text = message;
+	Util.TweenFastToSlow(self,"modulate:a",1,0.25,func(): Util.setTime(1,func(): pass))
+
+func tween_ani():
+	Util.setTime(1,func():Util.TweenSlowToFast(self,"modulate:a",0,0.25,func(): pass))
 	
