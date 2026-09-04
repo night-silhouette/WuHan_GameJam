873e68918a06be08300eebe30f1e87c32f683521
night <631434665@qq.com>
2026-09-05 04:17:11 +0800

    暂存

--- src/Code/Scene/field/field.gd
+++ src/Code/Scene/field/field.gd
@@ -8,9 +8,15 @@ class_name FieldPerform
 @export var level:int=0
 @export var cropId:Const.CropId=Const.CropId.Nil
 
+
+@onready var toolbar: Sprite2D = $Toolbar
+
+
+var barFlag=false
 func _ready() -> void:
 	Util.Area2dConnectClick(area_2d,func():
 		if Mouse.mos==Mouse.ToolMode.SHOVEL:
-			print(123)
+			barFlag=!barFlag
+			toolbar.visible=barFlag
 		)
 		
