6e38033ef8ab581fb3777f8b14a6d03e74c5ab05
night <631434665@qq.com>
2026-09-06 00:39:57 +0800

    修理

--- src/Code/Ui/farm_ui/goods_ui.gd
+++ src/Code/Ui/farm_ui/goods_ui.gd
@@ -1,11 +1,10 @@
 extends Control
 
-@export var texture1 : Texture
+
 var num
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$TextureRect.texture = texture1
+
+	
 	
 	
 func update(num:String):
