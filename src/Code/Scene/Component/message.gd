36347dfb5df8016ff350d116caf3a2ea2ec7a36d
Napori232 <3093291282@qq.com>
2026-09-06 09:03:35 +0800

    0.1

--- src/Code/Scene/Component/message.gd
+++ src/Code/Scene/Component/message.gd
@@ -1,16 +1,15 @@
 extends TextureRect
-var labfix : = LabelFix
+@onready var labfix : = $Label
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.MessagePopu.connect(MessagePopu)
-	modulate.a = 0;
 	$".".position.y = 460
 
 func MessagePopu(message:String):
-	labfix.auto_text = message;
+	labfix.text = message;
 	Util.TweenFastToSlow(self,"position:y",265,0.25,tween_ani)
 
 func tween_ani():
-	Util.setTime(3,func():Util.TweenSlowToFast(self,"position:y",265,0.25,func(): pass))
+	Util.setTime(1,func():Util.TweenSlowToFast(self,"position:y",460,0.25,func(): pass))
 	
